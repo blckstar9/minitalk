ad64ff589b4e561f1f58a5feee82b7fb2e03910d
blckstar9 <belaouchiayoub@gmail.com>
2025-02-10 21:36:10 +0100

    added inc and srcs directories

---
+++ inc/minitalk.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aybelaou <aybelaou@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 19:44:55 by aybelaou          #+#    #+#             */
+/*   Updated: 2025/02/10 20:37:04 by aybelaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include "../libft/inc/libft_inc/libft.h"
+#include "../libft/inc/gnl_inc/get_next_line.h"
+#include "../libft/inc/printf_inc/ft_printf.h"
+
+# define RED "\033[31m"
+# define RS "\033[0m"
+# define CY "\033[36m"
+# define YL "\033[33m"
+# define GR "\033[32m"
+# define MG "\033[35m"
+
+#endif
