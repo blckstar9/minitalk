74553b999540078dc1703ca62733490cee2ae9d2
blckstar9 <belaouchiayoub@gmail.com>
2025-03-06 18:10:28 +0100

    updated client file

--- srcs/client.c
+++ srcs/client.c
@@ -6,70 +6,82 @@
 /*   By: aybelaou <aybelaou@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:57:32 by aybelaou          #+#    #+#             */
-/*   Updated: 2025/03/03 17:38:18 by aybelaou         ###   ########.fr       */
+/*   Updated: 2025/03/06 18:09:42 by aybelaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-// // Sends the specified message to the specified
-// // process using SIGUSR1 and SIGUSR2 signals
-// // Iterate through each character of the message
-// // Iterate through each bit of the current character
-// // If the current bit is 1, send SIGUSR1 signal;
-// // otherwise, send SIGUSR2 signal
-// // Sleep for 100 microseconds between each signal
-// void	signal_action(int pid, char *str)
-
-
-int	g_server_pid = 0;
-
-void	signal_action(int pid, char *str)
-{
-	int	i;
-	int	c;
-
-	while (*str)
-	{
-		c = *(str);
-		i = 0;
-		while (i < 8)
-		{
-			if (c & 0b10000000)
-			{
-				if (kill(pid, SIGUSR1) == -1)
-					ft_printf("Unable to send SIGUSR1\n");
-			}
-			else
-			{
-				if (kill(pid, SIGUSR2) == -1)
-					ft_printf("Unable to send SIGUSR2\n");
-			}
-			c <<= 1;
-			usleep(100);
-			i++;
-		}
-		str++;
-	}
-}
+volatile sig_atomic_t	g_sig_received = 0;
 
 void	handler(int sig)
 {
-	static int	message_printed = 0;
-
-	if (!message_printed)
+	if (sig == SIGUSR1)
 	{
-		if (sig == SIGUSR1)
-		{
-			ft_printf("The signal was received successfully.\n");
-			message_printed = 1;
-		}
+		ft_printf(GR"Character received successfully.\n"RS);
 	}
+	if (sig == SIGUSR1 || sig == SIGUSR2)
+	{
+		g_sig_received = 1;
+	}
+}
+
+int	send_bit(int pid, int bit)
+{
+	int	timeout_count;
+
+	timeout_count = 0;
+	g_sig_received = 0;
+	if (bit)
+	{
+		if (kill(pid, SIGUSR1) == -1)
+			return (ft_putstr_fd(RED"Error: Unable to send SIGUSR1\n"RS, 2), 0);
+	}
+	else
+	{
+		if (kill(pid, SIGUSR2) == -1)
+			return (ft_putstr_fd(RED"Error: Unable to send SIGUSR2\n"RS, 2), 0);
+	}
+	while (!g_sig_received && timeout_count < 1000)
+	{
+		usleep(100);
+		timeout_count++;
+	}
+	if (timeout_count >= 1000)
+		return (ft_putstr_fd(RED"Error: Server timeout - retrying\n"RS, 2), 0);
+	return (1);
+}
+
+void	send_char(int pid, char c)
+{
+	int	i;
+	int	bit;
+
+	i = 0;
+	while (i < 8)
+	{
+		bit = !!(c & (0x80 >> i));
+		if (!send_bit(pid, bit))
+			i--;
+		else
+			i++;
+	}
+}
+
+void	signal_action(int pid, char *str)
+{
+	while (*str)
+	{
+		send_char(pid, *str);
+		str++;
+	}
+	send_char(pid, '\0');
 }
 
 int	main(int argc, char **argv)
 {
 	struct sigaction	sa_sig;
+	int					pid;
 	int					i;
 
 	if (argc != 3 || !argv[2])
@@ -80,15 +92,17 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (argv[1][i])
 		if (!ft_isdigit(argv[1][i++]))
-			if (argv[1][i++] == '\0')
-				return (ft_printf("Invalid PID.\n"), EXIT_FAILURE);
-	g_server_pid = ft_atoi(argv[1]);
-	if (g_server_pid <= 0)
-		return (ft_printf("Invalid PID.\n"), EXIT_FAILURE);
+			return (ft_putstr_fd(RED"Invalid PID.\n"RS, 2), EXIT_FAILURE);
+	pid = ft_atoi(argv[1]);
+	if (pid <= 0)
+		return (ft_putstr_fd(RED"Invalid PID.\n"RS, 2), EXIT_FAILURE);
 	sa_sig.sa_handler = &handler;
-	sa_sig.sa_flags = SA_SIGINFO;
+	sa_sig.sa_flags = 0;
+	sigemptyset(&sa_sig.sa_mask);
 	sigaction(SIGUSR1, &sa_sig, NULL);
 	sigaction(SIGUSR2, &sa_sig, NULL);
-	signal_action(g_server_pid, argv[2]);
+	ft_printf(GR"Sending message to PID %d...\n"RS, pid);
+	signal_action(pid, argv[2]);
+	ft_printf(GR"Message sent successfully.\n"RS);
 	return (EXIT_SUCCESS);
 }
