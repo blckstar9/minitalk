8a048e4f246c7a4bbcc47c9eab1bf9aedea1c334
blckstar9 <belaouchiayoub@gmail.com>
2025-03-06 18:21:27 +0100

    updated header file

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,21 +6,20 @@
 /*   By: aybelaou <aybelaou@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:44:55 by aybelaou          #+#    #+#             */
-/*   Updated: 2025/02/26 23:33:55 by aybelaou         ###   ########.fr       */
+/*   Updated: 2025/03/06 18:13:10 by aybelaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include "../libft/inc/libft_inc/libft.h"
-#include "../libft/inc/gnl_inc/get_next_line.h"
-#include "../libft/inc/printf_inc/ft_printf.h"
-//# include <bits/types/siginfo_t.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include "../libft/inc/libft_inc/libft.h"
+# include "../libft/inc/gnl_inc/get_next_line.h"
+# include "../libft/inc/printf_inc/ft_printf.h"
 
 # define RED "\033[31m"
 # define RS "\033[0m"
@@ -29,9 +28,10 @@
 # define GR "\033[32m"
 # define MG "\033[35m"
 
-typedef struct {
-    int bit_count;
-    unsigned char character;
-} t_state;
+typedef struct s_state
+{
+	int				bit_count;
+	unsigned char	character;
+}	t_state;
 
 #endif
